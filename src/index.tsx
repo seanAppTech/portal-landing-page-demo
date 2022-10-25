26951a80013762f62e00a46cd9f11db6d14e2fa2
seanAppTech <sstanden@apptechcorp.com>
2022-10-25 20:26:09 +0000

    Added colors.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from '@emotion/react';
+import { ThemeProvider } from '@mui/material/styles';
 
 import { theme } from './theme/theme';
 
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,12 +1,21 @@
-export const theme = {
-    colors: {
-        primary: '#2C28A2',
-        light: '#E8EBF5',
-        text: '#272727',
-        lightText: '#707070',
-        warning: '#FFC107',
-        error: '#D82C38',
-        success: '#00B69B',
-        blue: '#1976D2'
-    }
-}
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#2C28A2'
+        },
+        secondary: {
+            main: '#1976D2'
+        },
+        warning: {
+            main: '#FFC107'
+        },
+        error: {
+            main: '#D82C38'
+        },
+        success: {
+            main: '#00B69B'
+        },
+    },
+});
